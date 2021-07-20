4b2d6541b733b556154d4259f3c6228aea98751a
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-07-20 21:47:30 +0530

    add routes and header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import Footer from './components/Header_footer/Footer';
+import Header from './components/Header_footer/Header';
+import Home from './components/Home/index';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Header />
+            <Switch>
+                <Route path='/' exact>
+                    <Home />
+                </Route>
+                <Route path='/users' component=''></Route>
+                <Route path='/'></Route>
+            </Switch>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
