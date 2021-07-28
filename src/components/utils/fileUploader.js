3ec9cfdfcd4d036a37f5f61849b8b3cf2d142bec
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-07-28 23:54:03 +0530

    add logic for getting players image from firebase

--- src/components/utils/fileUploader.js
+++ src/components/utils/fileUploader.js
@@ -27,23 +27,57 @@ class FileUpload extends Component {
             name: filename,
             isUploading: false,
         });
+
+        firebase
+            .storage()
+            .ref(this.props.dir)
+            .child(filename)
+            .getDownloadURL()
+            .then(url => this.setState({ fileURL: url }));
     };
 
     render() {
         console.log(this.state);
         return (
             <div>
-                <div>
-                    <FileUploader
-                        accept='image'
-                        name='image'
-                        randomizeFilename
-                        storageRef={firebase.storage().ref(this.props.dir)}
-                        onUploadStart={this.handleUploadStart}
-                        onUploadError={this.handleUploadError}
-                        onUploadSuccess={this.handleUploadSuccess}
-                    />
-                </div>
+                {!this.state.fileURL ? (
+                    <div>
+                        <FileUploader
+                            accept='image'
+                            name='image'
+                            randomizeFilename
+                            storageRef={firebase.storage().ref(this.props.dir)}
+                            onUploadStart={this.handleUploadStart}
+                            onUploadError={this.handleUploadError}
+                            onUploadSuccess={this.handleUploadSuccess}
+                        />
+                    </div>
+                ) : null}
+
+                {this.state.isUploading ? (
+                    <div
+                        className='progress'
+                        style={{ textAlign: 'center', margin: '30px 0' }}
+                    >
+                        <CircularProgress
+                            stle={{ color: '#98c6e9' }}
+                            thickness={7}
+                        />
+                    </div>
+                ) : null}
+
+                {this.state.fileURL ? (
+                    <div className='image_upload_container'>
+                        <img
+                            src={this.state.fileURL}
+                            width='100%'
+                            alt='player'
+                        />
+                        <div className='remove' onClick={''}>
+                            Remove
+                        </div>
+                    </div>
+                ) : null}
             </div>
         );
     }
